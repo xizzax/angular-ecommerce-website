c496f97f333a70a3c986733846781709290ddfe0
farhat-is-coding <farhxt.abbxs69@gmail.com>
2023-08-25 04:53:23 +0500

    not found page animation

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,9 +1,37 @@
 import { Component } from '@angular/core';
+import {
+  trigger,
+  transition,
+  style,
+  animate,
+  query,
+  stagger,
+  animateChild,
+  state,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.css']
+  styleUrls: ['./not-found.component.css'],
+  animations:[
+    trigger('staggerAnimation', [
+      transition(':enter', [
+        query(
+          '@fadeIn',
+          [stagger(100, [animateChild()])], { optional: true }),
+      ]),
+    ]),
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'scale(0)' }),
+        animate(
+          '300ms ease-in-out',
+          style({ opacity: 1, transform: 'scale(1)' })
+        ),
+      ]),
+    ]),
+  ]
 })
 export class NotFoundComponent {
 
